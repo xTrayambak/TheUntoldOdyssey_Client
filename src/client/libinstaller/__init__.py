ebb0d8d8c9c8bcfb41bcec38c04d18e5390912ee
xTrayambak <manishsowa725@gmail.com>
2021-12-27 11:16:01 +0530

    libinstaller v3

---
+++ src/client/libinstaller/__init__.py
@@ -0,0 +1,24 @@
+from src.client.log import log
+
+from subprocess import check_call as call
+from sys import executable
+from pkg_resources import get_distribution, DistributionNotFound
+
+def exists(package: str):
+    try:
+        dist = get_distribution(package)
+        return True
+    except DistributionNotFound:
+        return False
+
+def installAllLibraries():
+    libs = open("assets/requirements").readlines()
+
+    for lib in libs:
+        lib = lib.split("\n")[0]
+        if exists(lib): return
+        log("Installing library [{}]; querying PyPi.")
+        
+        call(
+            [executable, "-m", "pip", "install", lib]
+        )
